00b9b5e54a17d44f4ceb4140bd467981285eeaf2
techbooster23 <akashp8405@gmail.com>
2025-01-31 14:54:08 +0530

    add a post route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.send("Hello World!");
 }); 
+
+app.post("/",(req,res)=>{
+    console.log(req.body.name);
+    res.status(200);
+    
+});
 
 app.listen(3000,()=>{
     console.log("app is running at port 3000");
